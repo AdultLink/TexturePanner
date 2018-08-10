b1eae880e36900584d6bc16a81642e11f660a8e2
Willy Angel <gguillermix@gmail.com>
2018-08-10 03:04:42 +0200

    Rainbow thruster, new rings gif

---
+++ Assets/AdultLink/TexturePanner/Examples/Scripts/HueShift.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HueShift : MonoBehaviour {
+
+	// Use this for initialization
+
+	private Material mat;
+	public float speed = 5f;
+	public Color color;
+	void Start () {
+
+		mat = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+			float hue;
+			float sat;
+			float value;
+			Color.RGBToHSV(color, out hue, out sat, out value);
+			mat.SetColor("_Color", color);
+			hue += Time.fixedDeltaTime * speed;
+
+			if (hue > 1.0f) {
+				hue = 0f;
+			}
+
+			if (hue < 0f) {
+				hue = 1.0f;
+			}
+			color = Color.HSVToRGB(hue,sat,1.3f);
+	}
+}
