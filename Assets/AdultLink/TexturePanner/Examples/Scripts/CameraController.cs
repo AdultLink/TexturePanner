dbae67eb7480dda552d723ad6fc7c4e490506484
Willy Angel <gguillermix@gmail.com>
2018-09-19 16:15:59 +0200

    Minor tweak

--- Assets/AdultLink/TexturePanner/Examples/Scripts/CameraController.cs
+++ Assets/AdultLink/TexturePanner/Examples/Scripts/CameraController.cs
@@ -47,7 +47,6 @@ namespace AdultLink {
 			//FREE VIEW
 			if (freeView){
 				if (lockCursor){
-					movementSpeed = Mathf.Max(movementSpeed += Input.GetAxis("Mouse ScrollWheel"), 0.0f);
 					transform.position += (transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical")) * movementSpeed;
 					transform.eulerAngles += new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0f);
 					detectElement();
